834cd3ef3c5ea7ed30e9e756c0265aa3f1b8d871
Arthur Sarmento <arthurbs17@gmail.com>
2021-07-15 22:00:54 -0300

    Jokempo concluido

---
+++ script.js
@@ -0,0 +1,139 @@
+const buttonRock = document.getElementById('rock');
+const buttonPaper = document.getElementById('paper');
+const buttonScissors = document.getElementById('scissors')
+
+
+function playerChoice (n){
+
+    let choice = n.value
+
+    return choice
+}
+
+function computerChoice (){
+
+    let min = 1;
+    let max = 3;
+    let numberChoice = Math.floor(Math.random() * (max - min + 1)) + min;
+    let choice = '';
+
+    if (numberChoice === 1){
+        choice = 'pedra'
+    }
+
+    if (numberChoice === 2){
+        choice = 'papel'
+    }
+
+    if (numberChoice === 3){
+        choice = 'tesoura'
+    }
+
+    return choice
+}
+
+
+function rockDisplay(){
+
+    let choiceComputer = computerChoice();
+    let winner = ''
+
+    if (playerChoice(buttonRock) && choiceComputer === 'tesoura'){
+        console.log(choiceComputer);
+        winner = 'O Computador escolheu tesoura, Player wins!'
+    }
+
+    if (playerChoice(buttonRock) && choiceComputer === 'pedra'){
+        console.log(choiceComputer);
+        winner = 'O Computador também escolheu pedra, Empatou!'
+    }
+
+    if (playerChoice(buttonRock) && choiceComputer === 'papel'){
+        console.log(choiceComputer);
+        winner = 'O Computador escolheu papel, Computer wins!'
+    }
+
+    console.log(winner)
+    return winner
+}
+
+function paperDisplay(){
+
+    let choiceComputer = computerChoice();
+    let winner = ''
+
+    if (playerChoice(buttonPaper) && choiceComputer === 'pedra'){
+        console.log(choiceComputer);
+        winner = 'O Computador escolheu pedra, Player wins!'
+    }
+    if (playerChoice(buttonPaper) && choiceComputer === 'tesoura'){
+        console.log(choiceComputer);
+        winner = 'O Computador escolheu tesoura, Computer wins!'
+    }
+    if (playerChoice(buttonPaper) && choiceComputer === 'papel'){
+        console.log(choiceComputer);
+        winner = 'O Computador também escolheu papel, Empatou!'
+    }
+
+    console.log(winner)
+    return winner
+}
+
+function scissorsDisplay(){
+    
+    let choiceComputer = computerChoice();
+    let winner = ''
+
+    if (playerChoice(buttonScissors) && choiceComputer === 'papel'){
+        console.log(choiceComputer);
+        winner = 'O Computador escolheu papel, Player wins!'
+    }
+    if (playerChoice(buttonScissors) && choiceComputer === 'pedra'){
+        console.log(choiceComputer);
+        winner = 'O Computador escolheu pedra, Computer wins!'
+    }
+    if (playerChoice(buttonScissors) && choiceComputer === 'tesoura'){
+        console.log(choiceComputer);
+        winner = 'O Computador escolheu tesoura, Empatou!'
+    }
+
+    console.log(winner)
+    return winner
+}
+
+
+function showResults(n){
+
+    let divNova = document.createElement("div");
+
+    let newp = document.createElement("p");
+
+    divNova.appendChild(newp);
+
+    let kata = document.createTextNode(n);
+    
+    newp.appendChild (kata);
+
+    let destination = document.getElementById("winner");
+
+    destination.appendChild(divNova);
+}
+
+
+buttonRock.addEventListener('click', function(){
+
+    let result = rockDisplay();
+    showResults(result);
+})
+
+buttonPaper.addEventListener('click', function(){
+
+    let result = paperDisplay();
+    showResults(result);
+})
+
+buttonScissors.addEventListener('click', function(){
+
+    let result = scissorsDisplay();
+    showResults(result);
+})
